806d9335a3ac971adb67150bf887ad004817e186
Autumn <adamhoffensetz@gmail.com>
2025-05-07 12:40:08 -0400

    Fix #6 on 1.20.4

--- src/main/java/com/atom596/titanium/mixin/GeodeFeatureMixin.java
+++ src/main/java/com/atom596/titanium/mixin/GeodeFeatureMixin.java
@@ -1,10 +1,9 @@
 package com.atom596.titanium.mixin;
 
-import com.atom596.titanium.Titanium;
+import com.atom596.titanium.util.GeodeConfigInterface;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.inventory.LootableInventory;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.StructureWorldAccess;
@@ -16,21 +15,24 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GeodeFeature.class)
-public class TitaniumMixin {
+public class GeodeFeatureMixin {
 	@Inject(at = @At("RETURN"), method = "generate")
 	private void gen_barrel(FeatureContext<GeodeFeatureConfig> context, CallbackInfoReturnable<Boolean> cir) {
-		StructureWorldAccess structureWorldAccess = context.getWorld();
-		Random random = context.getRandom();
-		int radius = context.getConfig().outerWallDistance.get(random);
+		if (((GeodeConfigInterface) context.getConfig()).getBarrelLootTable() != null) {
+			StructureWorldAccess structureWorldAccess = context.getWorld();
+			Random random = context.getRandom();
+			int radius = context.getConfig().outerWallDistance.get(random);
 
-		BlockPos barrelPos = context.getOrigin().add(radius, 0, radius);
-		for(int i = 0; i < 2 * radius; i++) {
-			if(!structureWorldAccess.isAir(barrelPos)) {
-				barrelPos = barrelPos.add(0, 1, 0);
+			BlockPos barrelPos = context.getOrigin().add(radius, 0, radius);
+			for(int i = 0; i < 2 * radius; i++) {
+				if(!structureWorldAccess.isAir(barrelPos)) {
+					barrelPos = barrelPos.add(0, 1, 0);
+				}
 			}
-		}
 
-		structureWorldAccess.setBlockState(barrelPos, Blocks.BARREL.getDefaultState(), Block.NOTIFY_LISTENERS);
-		LootableInventory.setLootTable(structureWorldAccess, random, barrelPos, new Identifier(Titanium.MOD_ID, "chests/amethyst_barrel"));
+			structureWorldAccess.setBlockState(barrelPos, Blocks.BARREL.getDefaultState(), Block.NOTIFY_LISTENERS);
+			LootableInventory.setLootTable(structureWorldAccess, random, barrelPos,
+					((GeodeConfigInterface) context.getConfig()).getBarrelLootTable());
+		}
 	}
 }
---
+++ src/main/java/com/atom596/titanium/util/GeodeConfigInterface.java
@@ -0,0 +1,8 @@
+package com.atom596.titanium.util;
+
+import net.minecraft.util.Identifier;
+
+public interface GeodeConfigInterface {
+    Identifier getBarrelLootTable();
+    void setBarrelLootTable(Identifier id);
+}
