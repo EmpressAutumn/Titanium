d3b053889466c7a08ec78d05a4fdf4f25fca26e5
Autumn <adamhoffensetz@gmail.com>
2025-09-24 12:13:15 -0400

    Flight Charge buff v1

--- common/src/main/java/com/atom596/titanium/item/FlightChargeItem.java
+++ common/src/main/java/com/atom596/titanium/item/FlightChargeItem.java
@@ -7,6 +7,8 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.flag.FeatureFlagSet;
@@ -25,16 +27,32 @@ public class FlightChargeItem extends Item implements ProjectileItem {
     public InteractionResultHolder<ItemStack> use(Level world, Player user, InteractionHand hand) {
         world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.WIND_CHARGE_BURST.value(), SoundSource.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
         ItemStack itemStack = user.getItemInHand(hand);
+
+        // Change user trajectory
         if (user.getY() < world.getMinBuildHeight()) {
             user.getCooldowns().addCooldown(this, 35);
-            user.setDeltaMovement(user.getDeltaMovement().add(new Vec3(0, 4, 0)));
+            user.setDeltaMovement(user.getDeltaMovement().add(new Vec3(0, 6, 0)));
         } else {
             user.getCooldowns().addCooldown(this, 20);
-            user.setDeltaMovement(user.getDeltaMovement().add(new Vec3(0, 1.5, 0)));
+            user.setDeltaMovement(user.getDeltaMovement().add(new Vec3(0, 2, 0)));
         }
         if (user.getDeltaMovement().y < 0) {
             user.setDeltaMovement(new Vec3(0, 0, 0));
         }
+
+        // Add or remove effects
+        if (user.hasEffect(MobEffects.LEVITATION)) {
+            user.removeEffect(MobEffects.LEVITATION);
+        }
+        if (user.hasEffect(MobEffects.SLOW_FALLING)) {
+            if (user.getEffect(MobEffects.SLOW_FALLING).getDuration() < 50) {
+                user.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 50));
+            }
+        } else {
+            user.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 50));
+        }
+
+        // Change item stats
         user.increaseScore(Stats.ITEM_USED.getRegistry().getId(this));
         if (!user.isCreative()) {
             user.getItemInHand(hand).shrink(1);
